592fda70de879ddffde172c23834163de93934f1
Sachin Dua <sachindua53@gmail.com>
2022-01-19 06:24:10 -0800

    little adjustments

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package Sorting;
+public class SelectionSort 
+{
+    public static void main(String[] args) {
+        int[] arr={5,2,8,1,6};
+        selectionSort(arr);
+
+        for(int x:arr)
+        {
+            System.out.print(x+" ");
+        }
+
+    }
+    static void selectionSort(int []arr)
+    {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex=i;
+
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j]<arr[minIndex])
+                {
+                    minIndex=j;
+                }
+            }
+
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+        }
+    }
+
+}
