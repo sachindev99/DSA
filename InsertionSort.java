c45ef7831bf5d0cdaa3323e097e56f7b6e3c7848
Sachin Dua <sachindua53@gmail.com>
2022-01-18 22:05:43 -0800

    init

---
+++ InsertionSort.java
@@ -0,0 +1,31 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        int n =arr.length;
+        insertSort(arr, n);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    static void insertSort(int[] arr,int n)
+    {
+        for (int i = 0; i < n; i++) {
+            //store the current element in a key variable
+            int key=arr[i];
+            int j=i-1;
+
+            //shift element to the the right until there's an element smaller than key
+            while(j>=0 && arr[j]>key)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            //when you find an element smaller than key you insert the element to its right
+
+            arr[j+1]=key;
+        }
+    }
+    
+}
