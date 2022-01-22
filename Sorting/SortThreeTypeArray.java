b2293ea35de8b3ea37a87c59029d4e2021c816c2
Sachin Dua <sachindua53@gmail.com>
2022-01-22 09:30:48 -0800

    Sort an array with three types of elements

---
+++ Sorting/SortThreeTypeArray.java
@@ -0,0 +1,49 @@
+package Sorting;
+
+public class SortThreeTypeArray
+{
+    public static void main(String[] args) {
+        int[]arr={0,1,0,2,1,2};
+        sort(arr);
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+            
+        }
+    }
+
+    static void sort(int[]arr)
+    {
+        int low=0;
+        int high=arr.length-1;
+        int mid=0;
+
+        while(mid<=high)
+        {
+            if(arr[mid]==0)
+            {
+                int temp=arr[mid];
+                arr[mid]=arr[low];
+                arr[low]=temp;
+                low++;
+                mid++;
+
+            }
+
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+
+            else
+            {
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+            }
+
+
+        }
+    }
+}
