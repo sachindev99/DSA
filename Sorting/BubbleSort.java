0b5d2f1484d92344590dcfb2b467e11f143c138d
Sachin Dua <sachindua53@gmail.com>
2022-02-05 16:07:03 -0800

    improvisation

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,7 +2,7 @@ package Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr={15,1,10,8,2,7};
+        int[] arr={5,1,2,3,4};
         int n=arr.length;
         bubbleSort(arr, n);
         for (int i : arr) {
@@ -14,14 +14,21 @@ public class BubbleSort {
     {
         for(int i=0;i<n-1;i++)
         {
-            for(int j=0;j<n-1;j++)
+            boolean swapped= false;
+            for(int j=0;j<n-i-1;j++)
             {
                 if(arr[j]>arr[j+1])
                 {
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                 }
+            }
+
+            if(swapped==false)
+            {
+                break;
             }
         }
     }
