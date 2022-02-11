0b04ca3294681aead787750ff66f048e07588f64
Sachin Dua <sachindua53@gmail.com>
2022-02-10 21:31:40 -0800

    Merge Sort Improvisation

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -5,7 +5,6 @@ public class MergeSort {
         int[] arr={10,5,30,15,7};
         int n = arr.length;
         int low=0;int high=n-1;
-        
         mergeSort(arr, low, high);
 
         for (int i : arr) {
@@ -16,62 +15,63 @@ public class MergeSort {
 
     static void mergeSort(int[] arr, int low, int high)
     {
+        int temp[]= new int[arr.length];
         if(low<high)
         {
             int mid=(low+high)/2;
             mergeSort(arr, low, mid);
             mergeSort(arr, mid+1, high);
-            merge(arr,low,mid,high);
+            merge(arr,low,mid,high,temp);
         }
     }
 
-    static void merge(int[] arr, int low, int mid, int high) {
-        // low=0;
-        // high=arr.length-1;
-        // mid=(low+high)/2;
+    static void merge(int[] arr, int low, int mid, int high,int[] temp) {
         int i=low;
         int j= mid+1;
         int k=low;
-       // int temp[]= new int[arr.length];
+        
 
         while(i<=mid&&j<=high)
         {
-            if(arr[j]<=arr[i])
+            if(arr[i]<arr[j])
             {
-                int temp=arr[k];
-                arr[k]=arr[j];
-                arr[j]=temp;
+                temp[k]=arr[i];
+                i++;
                 k++;
-                j++;
             }
 
             else
             {
-                int temp=arr[k];
-                arr[k]=arr[i];
-                arr[i]=temp;
+                temp[k]=arr[j];
+                k++;
+                j++;
+            }
+        }
+
+        if(i>mid)
+        {
+            while(j<=high)
+            {
+                temp[k]=arr[j];
+                k++;
+                j++;
+            }
+          
+        }
+
+        else
+        {
+            while(i<=mid)
+            {
+                temp[k]=arr[i];
                 i++;
                 k++;
             }
         }
 
-        // while(i<=mid)
-        // {
-        //     int temp=arr[k];
-        //     arr[k]=arr[i];
-        //     arr[i]=temp;
-        //     i++;
-        //     k++;
-        // }
-        // while(j<=high)
-        // {
-        //     int temp=arr[k];
-        //     arr[k]=arr[j];
-        //     arr[j]=temp;
-        //     j++;
-        //     k++;
-        // }
-        
+        for (k = low; k<=high; k++) {
+            arr[k]=temp[k];
+        }
 
     }
     
