ffb1059bc1049ce2486d3cdb0651454b12b5b9ab
Sachin Dua <sachindua53@gmail.com>
2022-02-15 07:56:05 -0800

    Union of two sorted arrays

---
+++ Sorting/UnionOfTwoArrays.java
@@ -0,0 +1,81 @@
+package Sorting;
+
+public class UnionOfTwoArrays {
+    public static void main(String[] args) {
+        int[] arr={2,3,3,3,3,3,4,4};
+        int[] brr={3,3,4,4};
+        int n=arr.length;
+        int m =brr.length;
+        unionOfArrays(arr, brr, n, m);
+
+    }
+
+    static void unionOfArrays(int[] arr,int[] brr,int n,int m)
+    {
+        int i=0;
+        int j=0;
+
+        while(i<n&&j<m)
+        {
+            if(i>0&&arr[i]==arr[i-1])
+            {
+                i++;
+                continue;
+            }
+
+            if(j>0&&brr[j]==brr[j-1])
+            {
+                j++;
+                continue;
+            }
+
+            if(arr[i]<brr[j])
+            {
+                    System.out.print(arr[i]+" ");
+                    i++;
+            }
+
+            else if(arr[i]==brr[j])
+            {
+                System.out.print(arr[i]+" ");
+                i++;
+                j++;
+            }
+
+            else
+            {
+                System.out.print(brr[j]+" ");
+                j++;
+            }
+         
+        }
+
+        while(i<n)
+        {
+            if(i>0&&arr[i]!=arr[i-1])
+            {
+                System.out.print(arr[i]+" ");
+                i++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        while(j<m)
+        {
+            if(j>0&&brr[j]!=brr[j-1])
+            {
+                System.out.print(brr[j]+" ");
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+
+
+    }
+}
