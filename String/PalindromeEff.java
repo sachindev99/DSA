fbdd7c2d9476dc64ca0aae594c7b4a8602d86e52
Sachin Dua <sachindua53@gmail.com>
2022-04-19 05:19:15 -0700

    Plaindrome

---
+++ String/PalindromeEff.java
@@ -0,0 +1,27 @@
+package String;
+
+public class PalindromeEff {
+    public static void main(String[] args) {
+        String str="geeks";
+        System.out.println(isPlaindrome(str));
+    }
+
+    private static boolean isPlaindrome(String str) {
+        int i=0;
+        int j =str.length()-1;
+
+        while(i<j)
+        {
+            if(str.charAt(i)==(str.charAt(j)))
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
